63c9805ce9668e6b85675a76636fa68b1c2b59f5
JosHeredia26 <jose.heredia.pachas@gmail.com>
2021-11-25 16:58:28 -0500

    create card structure

---
+++ src/Components/Card.js
@@ -0,0 +1,29 @@
+function Card() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <div>
+          <div>
+            <p></p>
+            <p></p>
+          </div>
+          <p></p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+        <div>
+          <icon src=""></icon>
+          <p>250</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
